4c1f4ff5f06ca7f1282f981b81660fa2ed976901
jorgeoscanoa <jorge.oscanoa@neoland.es>
2025-06-27 12:45:13 +0200

    Initial project structure with folders and basic files

---
+++ src/app/shared/pipes/mardown/mardown.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mardown',
+  imports: [],
+  templateUrl: './mardown.component.html',
+  styleUrl: './mardown.component.css'
+})
+export class MardownComponent {
+
+}
