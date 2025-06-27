750783a9fd24d5d69aa79753866f69f55863b66b
jorgeoscanoa <jorge.oscanoa@neoland.es>
2025-06-27 13:49:40 +0200

    Create components routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { ChatComponent } from './features/chat/chat.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    //Ruta raiz que redirige automaticamente a chat
+    {path: '', redirectTo: 'chat', pathMatch: 'full'},
+    //Ruta /chat que renderiza ChatComponet
+    {path: 'chat', component: ChatComponent},
+    // Ruta comidin que redirige cualquier otra ruta a 'chat'
+    { path : '**', redirectTo: 'chat'}
+];
