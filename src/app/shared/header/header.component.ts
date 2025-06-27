f94750c40adda251eb58b65472d734d0acfd26e4
jorgeoscanoa <jorge.oscanoa@neoland.es>
2025-06-27 16:04:37 +0200

    Set up header component

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { ToolbarModule } from 'primeng/toolbar';
+import { RippleModule } from 'primeng/ripple';
 
 @Component({
   selector: 'app-header',
-  imports: [],
-  templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  imports: [
+    ToolbarModule,
+    ButtonModule,
+    RippleModule
+  ],
+  templateUrl: './header.component.html', //Archivo de plantilla HTML
+  styleUrls: ['./header.component.css'], // Archivo de stilos css
+  encapsulation: ViewEncapsulation.None // Desabilita encapsulacion para poder aplicar estilos globalos
 })
 export class HeaderComponent {
+  //Bandera que indica el modo oscuro (true) o claro (false)
+  darkMode = false;
 
+  // Alterna el tema de la aplicacion entre claro y oscuro
+  toggleTheme(){
+    this.darkMode = !this.darkMode; // cambia el estado de darkMode
+    document.body.classList.toggle('dark', this.darkMode);// Aplica o remueve clase en el body
+  }
 }
