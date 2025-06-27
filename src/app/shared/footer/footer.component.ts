18d774017fb85a727122a0b49ae767d52d2ca06f
jorgeoscanoa <jorge.oscanoa@neoland.es>
2025-06-27 16:18:38 +0200

    Set up footer component

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { ToolbarModule } from 'primeng/toolbar';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [ToolbarModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
-
+  currentYear = new Date().getFullYear();
 }
